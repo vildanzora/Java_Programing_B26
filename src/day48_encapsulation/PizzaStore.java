825f9ed42f4466a616591fc545d64a29d9760a8e
vildanzora <orgunvildan@gmail.com>
2022-04-19 15:23:15 -0500

    update

---
+++ src/day48_encapsulation/PizzaStore.java
@@ -0,0 +1,28 @@
+package day48_encapsulation;
+
+public class PizzaStore {
+    public static void main(String[] args) {
+
+        Pizza first = new Pizza ("large", 3);
+        System.out.println(first);
+
+        //first.setNumberOfToppings(2);
+        first.setNumberOfToppings(2);
+        System.out.println(first);
+
+        Pizza second = new Pizza ("", -2);
+        System.out.println(second);
+
+        second.setSize("Small");
+        second.setNumberOfToppings(1);
+        System.out.println(second);
+
+        String size = "small";
+        System.out.println(size.equalsIgnoreCase("small") ? 4 : size.equalsIgnoreCase("medium") ? 6 : 8);
+
+
+
+
+    }
+
+}
