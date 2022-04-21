f0fa02fe5453642c3603d035a510dd8216b5734d
vildanzora <orgunvildan@gmail.com>
2022-04-21 13:23:44 -0500

    class

--- src/day15_switch/ExampleForAuthor.java
+++ src/day15_switch/ExampleForAuthor.java
@@ -2,7 +2,7 @@ package day15_switch;
 
 import java.util.Scanner;
 
-public class Author {
+public class ExampleForAuthor {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
---
+++ src/day50_inheritance/book/Book.java
@@ -0,0 +1,20 @@
+package day50_inheritance.book;
+
+import day15_switch.ExampleForAuthor;
+
+public class Book {
+
+    String title;
+    Author author;
+    String genre;
+    int chapters;
+    double price;
+    boolean hasAMovie;
+
+}
+ /*
+ Create a book class
+
+ -create variables;
+ tittle, author, genre, chapter, price, has a movie
+  */
